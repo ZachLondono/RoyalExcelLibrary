587e1ed55014b689ab04026f287e3a18226c5c66
ZachLondono <zachary.londono5@gmail.com>
2021-10-28 11:57:47 -0400

    Added option for generating and printing cutlist

--- RoyalExcelLibrary/src/ExcelLibrary.cs
+++ RoyalExcelLibrary/src/ExcelLibrary.cs
@@ -9,20 +9,22 @@ using Excel = Microsoft.Office.Interop.Excel;
 using ExcelDna.Integration;
 using System.Windows.Forms;
 using RoyalExcelLibrary.ExportFormat.Labels;
+using RoyalExcelLibrary.ExportFormat;
+using Microsoft.Office.Interop.Excel;
 
 namespace RoyalExcelLibrary {
 	public class ExcelLibrary {
 
         #if DEBUG
             public const string db_path = "R:\\DB ORDERS\\RoyalExcelLibrary\\JobsTesting.db";
-        #else
+#else
             public const string db_path = "R:\\DB ORDERS\\RoyalExcelLibrary\\Jobs.db";
-        #endif
+#endif
 
-        public static void DrawerBoxProcessor(string format, bool trackjob, bool generateCutLists, bool printLabels, bool printCutlists) {
+        public static void DrawerBoxProcessor(string format, bool trackjob, bool generateCutLists, bool printLabels, bool printCutlists, bool generatePackingList, bool printPackingList) {
 
 #if DEBUG
-    MessageBox.Show($"Starting in Debug Mode\n Using database: '{db_path}'");
+            MessageBox.Show($"Starting in Debug Mode\n Using database: '{db_path}'");
 #endif
 
             var app = ExcelDnaUtil.Application as Excel.Application;
@@ -50,72 +52,124 @@ namespace RoyalExcelLibrary {
             }
 
             Order order = provider.LoadCurrentOrder();
-            SqliteConnection dbConnection = new SqliteConnection($"Data Source='{db_path}'");
 
-            using (dbConnection) {
+            bool printerInstalled = false;
+            string printerName = "SHARP MX-M283N PCL6";
+            if (printCutlists || printPackingList) {
+                var printers = System.Drawing.Printing.PrinterSettings.InstalledPrinters;
 
-                var productService = new DrawerBoxService(dbConnection);
-                var inventoryService = new InventoryService(dbConnection);
-                IEnumerable<Part> unplacedParts = null;
-
-                if (trackjob) {
-                    dbConnection.Open();
-                    order = productService.StoreCurrentOrder(order);
-                    inventoryService.TrackMaterialUsage(order, out unplacedParts);
-                    dbConnection.Close();
-
-                    if (unplacedParts != null) {
-                        string unplaced = "";
-                        foreach (Part part in unplacedParts)
-                            unplaced += $"{part.Qty}x{part.Width}Wx{part.Length}L {part.Material}\n";
-
-                        if (!string.IsNullOrEmpty(unplaced))
-                            MessageBox.Show("Unable to find available inventory for the following parts:\n" + unplaced, "Untracked Parts");
+                foreach (var printer in printers) {
+                    if (printer.Equals(printerName)) {
+                        printerInstalled = true;
+                        break;
                     }
                 }
+            }
 
-                if (generateCutLists) {
+            if (trackjob || generateCutLists) {
+                SqliteConnection dbConnection = new SqliteConnection($"Data Source='{db_path}'");
 
-                    try {
-                        app.ScreenUpdating = false;
-                        IProductService service = new DrawerBoxService(dbConnection);
+                using (dbConnection) {
+
+                    var productService = new DrawerBoxService(dbConnection);
+                    var inventoryService = new InventoryService(dbConnection);
+                    IEnumerable<Part> unplacedParts = null;
+
+                    if (trackjob) {
                         dbConnection.Open();
-                        var cutlists = service.GenerateCutList(order, app.ActiveWorkbook);
+                        order = productService.StoreCurrentOrder(order);
+                        inventoryService.TrackMaterialUsage(order, out unplacedParts);
                         dbConnection.Close();
 
-                        string printerName = "SHARP MX-M283N PCL6";
-                        var printers = System.Drawing.Printing.PrinterSettings.InstalledPrinters;
+                        if (unplacedParts != null) {
+                            string unplaced = "";
+                            foreach (Part part in unplacedParts)
+                                unplaced += $"{part.Qty}x{part.Width}Wx{part.Length}L {part.Material}\n";
 
-                        bool printerInstalled = false;
-                        foreach (var printer in printers) {
-                            if (printer.Equals(printerName)) {
-                                printerInstalled = true;
-                                break;
+                            if (!string.IsNullOrEmpty(unplaced))
+                                MessageBox.Show("Unable to find available inventory for the following parts:\n" + unplaced, "Untracked Parts");
+                        }
+                    }
+
+                    if (generateCutLists) {
+
+                        try {
+                            app.ScreenUpdating = false;
+                            IProductService service = new DrawerBoxService(dbConnection);
+                            dbConnection.Open();
+                            var cutlists = service.GenerateCutList(order, app.ActiveWorkbook);
+                            dbConnection.Close();
+
+                            if (!printerInstalled) {
+                                // TODO open popup for user to select printer
+                                throw new InvalidOperationException($"Unable to print.\nPrinter '{printerName}' not available");
                             }
-						}
 
-                        if (!printerInstalled) {
-                            // TODO open popup for user to select printer
-                            throw new InvalidOperationException($"Unable to print.\nPrinter '{printerName}' not available");
-						}
+                            if (printCutlists) {
+                                foreach (var cutlist in cutlists) {
+                                    //cutlist.PrintOut(ActivePrinter: printerName);
+                                    cutlist.PrintPreview();
+                                }
+                            }
 
-                        if (printCutlists) {
-                            foreach (var cutlist in cutlists) {
-                                cutlist.PrintOut(ActivePrinter: printerName);
-							}
-						}
+                            app.ScreenUpdating = true;
+                        } catch (Exception e) {
+                            app.ScreenUpdating = true;
+                            var result = MessageBox.Show($"An error occured while processing drawer boxes\nShow error message?\n[{e.Message}]", "Error occurred", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                            if (result == DialogResult.Yes) {
+                                MessageBox.Show(e.ToString(), "Error Message");
+                            }
+                        }
 
-                        app.ScreenUpdating = true;
-                    } catch (Exception e) {
-                        app.ScreenUpdating = true;
-                        var result = MessageBox.Show($"An error occured while processing drawer boxes\nShow error message?\n[{e.Message}]", "Error occurred", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-                        if (result == DialogResult.Yes) {
-                            MessageBox.Show(e.ToString(), "Error Message");
-					    }
+                    }
+
+
+                }
+            }
+
+            if (generatePackingList) {
+
+                try {
+                    PackingListExport packingListExp = new PackingListExport();
+
+                    PackingListData data = new PackingListData {
+
+                        SupplierName = "Metro Drawer Boxes",
+                        SupplierContact = "",
+                        SupplierAddress = new Address {
+                            StreetAddress = "15E Easy St",
+                            City = "Bound Brook",
+                            State = "NJ",
+                            Zip = "08805"
+                        },
+
+                        RecipientName = order.CustomerName,
+                        RecipientContact = "",
+                        RecipientAddress = order.ShipAddress
+
+                    };
+
+                    app.ScreenUpdating = false;
+                    Worksheet packingList = packingListExp.ExportOrder(order, data, app.ActiveWorkbook);
+                    app.ScreenUpdating = true;
+
+                    if (!printerInstalled) {
+                        // TODO open popup for user to select printer
+                        throw new InvalidOperationException($"Unable to print.\nPrinter '{printerName}' not available");
+                    }
+
+                    if (printPackingList) {
+                        //packingList.PrintOut(ActivePrinter: printerName);
+                        packingList.PrintPreview();
+                    }
+                } catch (Exception e) {
+                    app.ScreenUpdating = true;
+                    var result = MessageBox.Show($"An error occured while generating/printing the packing list\nShow error message?\n[{e.Message}]", "Error occurred", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                    if (result == DialogResult.Yes) {
+                        MessageBox.Show(e.ToString(), "Error Message");
                     }
 
                 }
-
 
             }
 
@@ -146,7 +200,7 @@ namespace RoyalExcelLibrary {
 			}
 
         }
-
-    }
+        
+	}
 
 }
