86c88225e89d2f7842dd2a4a4c473be8a5f680f3
ZachLondono <zachary.londono5@gmail.com>
2021-12-21 17:51:38 -0500

    Made DrawerBox's property setters private and created a DrawerBoxBuilder

--- src/Application/Features/Product/DrawerBox.cs
+++ src/Application/Features/Product/DrawerBox.cs
@@ -8,19 +8,28 @@ using System.Threading.Tasks;
 
 namespace RoyalExcelLibrary.Application.Features.Product {
 
-    internal class DrawerBox : ICompositeProduct<DrawerBoxPart> {
-        
-        public string Name => "Drawer Box";
+    public class DrawerBox : ICompositeProduct<DrawerBoxPart> {
 
-        public int Qty { get; set; }
-        public double Height { get; set; }
-        public double Width { get; set; }
-        public double Depth { get; set; }
-        public MaterialType BoxMaterial { get; set; }
-        public MaterialType BottomMaterial { get; set; }
+        internal DrawerBox(int qty, double height, double width, double depth, MaterialType boxMaterial, MaterialType bottomMaterial, Dictionary<string, string> extras) {
+            Qty = qty;
+            Height = height;
+            Width = width;
+            Depth = depth;
+            BoxMaterial = boxMaterial;
+            BottomMaterial = bottomMaterial;
+            Extras = extras;
+        }
+
+        public string Name => "Drawer Box";
+        public int Qty { get; private set; }
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+        public double Depth { get; private set; }
+        public MaterialType BoxMaterial { get; private set; }
+        public MaterialType BottomMaterial { get; private set; }
 
         // Extra options for the box
-        public List<string> Extras { get; set; }
+        public IReadOnlyDictionary<string, string> Extras { get; set; }
 
         public IList<DrawerBoxPart> GetParts() {
 
@@ -89,7 +98,7 @@ namespace RoyalExcelLibrary.Application.Features.Product {
 
     }
 
-    internal class DrawerBoxPart : IProduct {
+    public class DrawerBoxPart : IProduct {
 
         public MaterialType MatType { get; private set; }
 
@@ -111,6 +120,71 @@ namespace RoyalExcelLibrary.Application.Features.Product {
 
         public decimal Price() {
             return 0;
+        }
+
+    }
+
+    /// <summary>
+    /// Using the DrawerBoxBuilder allows for the creation of a drawer box step by step, and without the use of a unwieldy constructor while leaving the DrawerBox immutable
+    /// </summary>
+    internal class DrawerBoxBuilder {
+
+        private int _qty = 0;
+        private double _width = 0;
+        private double _height = 0;
+        private double _depth = 0;
+        private MaterialType _boxMaterial;
+        private MaterialType _bottomMaterial;
+        private Dictionary<string, string> _extras = new Dictionary<string, string>();
+
+        public DrawerBoxBuilder WithQty(int qty) {
+            _qty = qty;
+            return this;
+        }
+
+        public DrawerBoxBuilder WithWidth(double width) {
+            _width = width;
+            return this;
+        }
+
+        public DrawerBoxBuilder WithHeight(double height) {
+            _height = height;
+            return this;
+        }
+
+        public DrawerBoxBuilder WithDepth(double depth) {
+            _depth = depth;
+            return this;
+        }
+
+        public DrawerBoxBuilder WithBoxMaterial(MaterialType boxMaterial) {
+            _boxMaterial = boxMaterial;
+            return this;
+        }
+
+        public DrawerBoxBuilder DrawerBottomBuilder(MaterialType bottomMaterial) {
+            _bottomMaterial = bottomMaterial;
+            return this;
+        }
+
+        public DrawerBoxBuilder WithExtra(string key, string value) {
+            _extras.Add(key, value);
+            return this;
+        }
+        public DrawerBox Build() {
+
+            if (_qty < 1)                                   throw new InvalidOperationException("Quantity must be greater than 1");
+            if (_width < 1 || _height < 1 || _depth < 1)    throw new InvalidOperationException("Dimensions must be greater than 1");
+            if (_boxMaterial is null)                       throw new InvalidOperationException("Drawer box material must not be null");
+            if (_bottomMaterial is null)                    throw new InvalidOperationException("Drawer box material must not be null");
+
+            return new DrawerBox(_qty,
+                                _height,
+                                _width,
+                                _depth,
+                                _boxMaterial,
+                                _bottomMaterial,
+                                _extras);
         }
 
     }
