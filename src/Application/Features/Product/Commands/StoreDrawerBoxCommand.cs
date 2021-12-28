d9215780f60ca914b82db707591fe7de63b1bacb
ZachLondono <zachary.londono5@gmail.com>
2021-12-28 14:33:05 -0500

    Added a command to insert drawerbox into database

---
+++ src/Application/Features/Product/Commands/StoreDrawerBoxCommand.cs
@@ -0,0 +1,77 @@
+﻿using MediatR;
+using Dapper;
+using RoyalExcelLibrary.Application.Common;
+using System;
+using System.Data.OleDb;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoyalExcelLibrary.Application.Features.Product.Commands {
+    public class StoreDrawerBoxCommand : IRequest<DrawerBox> {
+        public DrawerBox Box { get; set; }
+        public int JobId { get; set; }
+    }
+
+    internal class StoreDrawerBoxCommandHandler : IRequestHandler<StoreDrawerBoxCommand, DrawerBox> {
+
+        private readonly DatabaseConfiguration _dbConfig;
+
+        public StoreDrawerBoxCommandHandler(DatabaseConfiguration dbConfig) {
+            _dbConfig = dbConfig;
+        }
+
+        public Task<DrawerBox> Handle(StoreDrawerBoxCommand request, CancellationToken cancellationToken) {
+
+            using (var connection = new OleDbConnection(_dbConfig.AppConfigConnectionString)) {
+
+                connection.Open();
+
+                // Insert the drawerbox 
+                int rows = connection.Execute(
+                    sql: @"INSERT INTO [DrawerBoxes] ([Qty], [Height], [Width], [Depth], [BoxMaterial], [BottomMaterial], [JobId])
+                            VALUES (@Qty, @Height, @Width, @Depth, @BoxMaterial, @BottomMaterial, @JobId);",
+                    param: new {
+                        Qty = request.Box.Qty,
+                        Height = request.Box.Height,
+                        Width = request.Box.Width,
+                        Depth = request.Box.Depth,
+                        BoxMaterial = request.Box.BoxMaterial.TypeId,
+                        BttomMaterial = request.Box.BottomMaterial.TypeId,
+                        JobId = request.JobId
+                    });
+
+
+                // Check that the drawerbox was inserted, and get it's Id
+                if (rows > 0) {
+                    var query = connection.CreateCommand();
+                    query.CommandText = "SELECT @@IDENTITY FROM DrawerBoxes;";
+                    var reader = query.ExecuteReader();
+                    reader.Read();
+                    request.Box.Id = reader.GetInt32(0);
+                } else {
+                    request.Box.Id = -1; // The new drawerbox was not inserted
+                    return Task.FromResult(request.Box);
+                }
+
+                // Insert the drawerbox's extra options
+                foreach (var extra in request.Box.Extras) {
+                    string category = extra.Key;
+                    string option = extra.Value;
+
+                    connection.Execute(
+                        sql: @"INSERT INTO [DrawerBoxExtras] ([Category], [Option], [ProductId])",
+                        param: new {
+                            Category = category,
+                            Option = option,
+                            ProductId = request.Box.Id
+                        });
+                }
+
+            }
+
+            return Task.FromResult(request.Box);
+
+        }
+    }
+
+}
