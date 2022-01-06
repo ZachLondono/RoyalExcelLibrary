f56c01b5f448f6373113af55fcec5cd69e41cfa9
ZachLondono <zachary.londono5@gmail.com>
2022-01-06 12:43:22 -0500

    Added test for html insert function

---
+++ tests/ExcelLibrary.Tests/Unit/EmailTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RoyalExcelLibrary.ExcelUI.ExportFormat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelLibrary.Tests.Unit {
+    public class EmailTests {
+
+
+        [Test]
+        public void Should_InsertIntoHtmlBody() {
+
+            string result = OutlookEmailExport.InsertIntoExistingBody("<html><body someattrb='abc'><p>Existing Content</p></body></html>", "Hello World");
+
+            result.Should().Be("<html><body someattrb='abc'><span>Hello World</span><p>Existing Content</p></body></html>");
+
+        }
+
+    }
+}
