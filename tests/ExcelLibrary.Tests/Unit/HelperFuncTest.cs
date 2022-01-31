f2241285932f30b9adafd8a239876c4a73a928d2
ZachLondono <zachary.londono5@gmail.com>
2022-01-31 16:27:22 -0500

    Updated tests for metric to inch rounding

--- tests/ExcelLibrary.Tests/Unit/HelperFuncTest.cs
+++ tests/ExcelLibrary.Tests/Unit/HelperFuncTest.cs
@@ -14,16 +14,18 @@ namespace ExcelLibrary.Tests.Unit {
         }
 
         [Test]
-        [TestCase(25.80, "1")]         // 1 1/64
-        [TestCase(26.19, "1")]         // 1 1/32
-        [TestCase(26.59, "1 1/16")]    // 1 3/64
+        [TestCase(25.00, "31/32")]     // 63/64
+        [TestCase(25.40, "1")]         // 1
+        [TestCase(25.80, "1 1/32")]    // 1 1/64
+        [TestCase(26.19, "1 1/32")]    // 1 1/32
+        [TestCase(26.59, "1 1/32")]    // 1 3/64
         [TestCase(26.99, "1 1/16")]    // 1 1/16
         [TestCase(27.38, "1 1/16")]    // 1 5/64
-        [TestCase(27.78, "1 1/16")]    // 1 3/32
+        [TestCase(27.78, "1 3/32")]    // 1 3/32
         [TestCase(28.18, "1 1/8")]     // 1 7/64
         [TestCase(28.58, "1 1/8")]     // 1 1/8
         [TestCase(28.97, "1 1/8")]     // 1 9/64
-        [TestCase(29.37, "1 3/16")]    // 1 5/32
+        [TestCase(29.37, "1 5/32")]    // 1 5/32
         [TestCase(29.77, "1 3/16")]    // 1 11/64
         public void Should_RoundToNearest16nd_WhenMetricIsNotExactlyA16nd(double metric, string expected) {
             string result = RoyalExcelLibrary.ExcelUI.HelperFuncs.FractionalImperialDim(metric);
