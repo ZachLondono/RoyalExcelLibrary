4935b19c120ccc72a4aaf6566c9d11c62e278324
ZachLondono <zachary.londono5@gmail.com>
2022-02-21 14:39:17 -0500

    Added more checks to make sure the correct value is being read.

--- src/RoyalExcelLibrary/src/XLExtension.cs
+++ src/RoyalExcelLibrary/src/XLExtension.cs
@@ -1,4 +1,5 @@
 ﻿using ClosedXML.Excel;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace RoyalExcelLibrary.ExcelUI {
@@ -26,11 +27,29 @@ namespace RoyalExcelLibrary.ExcelUI {
 
 		}
 
+		/// <summary>
+		/// Tries to read the value of the cell, and if it is a double that will be returned. Otherwise it will try to read the cached value, or convert the rich value into a double. 
+		/// </summary>
+		/// <param name="cell"></param>
+		/// <returns></returns>
 		public static double GetDoubleValue(this IXLCell cell) {
 
+			try {
+				object result = cell.Value;
+				if (result is null) {
+					Debug.WriteLine("Result is null");
+				} else if (result is double) {
+					return (double) result;
+				}
+			} catch {
+				Debug.WriteLine("Could not calculate value of cell");
+			}
+
 			if (cell.HasFormula) {
+				object cached = cell.CachedValue;
+				if (cached is double) return (double)cached;
 				return HelperFuncs.ConvertToDouble(
-					cell.CachedValue.ToString());
+					cached.ToString());
 			}
 
 			double richValue = HelperFuncs.ConvertToDouble(cell.RichText.ToString());
